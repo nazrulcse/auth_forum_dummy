87c4e80d4766a34afc298c6535721b3d1a5060ff
nazrul <nazrulku07@gmail.com>
2015-09-13 12:41:17 +0600

    set email configuration for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,18 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
 
+  config.action_mailer.default_url_options = {host: '52.25.197.167'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :domain               => "www.nascenia.com",
+      :user_name            => "do-not-reply@nascenia.com",
+      :password             => "12nascenia3",
+      :authentication       => "plain",
+      :enable_starttls_auto => true
+  }
+
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
